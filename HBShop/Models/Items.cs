1fb031773882bf2895997c91b27ec6ccac17f5a2
sahenasp <sahensir32@yahoo.com>
2014-07-22 21:40:04 +0600

    ad

---
+++ HBShop/Models/Items.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HBShop.Models
+{
+    public class Items
+    {
+        public long ID { get; set; }
+        public String ItemName { get; set; }
+        public String Size { get; set; }
+        public String ItemCode { get; set; }
+        public virtual Categorys category { get; set; }
+        public long UserID { get; set; }
+        public System.DateTime UpdateDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
