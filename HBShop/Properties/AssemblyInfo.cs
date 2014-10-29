e0bba19facdacc392a1ce6cbe766a9c3f50b16f9
m0n1 <monikarimcse@gmail.com>
2014-10-30 01:51:20 +0600

    nothing at all

--- HBShop/Properties/AssemblyInfo.cs
+++ HBShop/Properties/AssemblyInfo.cs
@@ -33,3 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+
+
+
+//Changed by Moni
